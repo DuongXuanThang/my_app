0e7d9390f60e8bd34e3f88470eadf09ac790a07f
DuongXuanThang <xuanthang1909972@gmail.com>
2023-12-04 16:30:41 +0700

    test_photos_app

---
+++ my_app/app_demo/doctype/photo_demo/photo_demo.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2023, VGM and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Photo_Demo(Document):
+	# def validate(self):
+    #     # TODO checklist:
+    #     # - check if file type is supported
+    #     # - extract and save image meta data
+    #     # - probably parse and save as JSON which can be updated via the UI and written to the file
+         pass
---
+++ my_app/app_demo/doctype/roi_demo/test_roi_demo.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, VGM and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestROI_Demo(FrappeTestCase):
+	pass
