0f59ddc3e163d406cbe6a8c65016db2fa5aea307
DuongXuanThang <xuanthang1909972@gmail.com>
2023-12-20 16:07:38 +0700

    check code

--- my_app/app_demo/doctype/article/article.js
+++ my_app/app_demo/doctype/article/article.js
@@ -4,4 +4,20 @@
 frappe.ui.form.on('Article', {
 	// refresh: function(frm) {
 	// }
+	image: function(frm) {
+        // Hành động khi trường 'your_image_field' thay đổi
+        var uploaded_file = frm.doc.image;
+
+        // Kiểm tra xem có tệp tin đã được tải lên không
+        if (uploaded_file) {
+			alert('123')
+            // Thực hiện hành động cập nhật cho trường khác, ví dụ: cập nhật trường 'another_field'
+            frm.set_value('namesession', 3);
+			
+        }else {
+            // Trường ảnh rỗng, thực hiện hành động cập nhật khác, ví dụ: đặt giá trị về 0
+            frm.set_value('namesession', 0);
+        }
+		frm.refresh_fields();
+    }
 });
--- my_app/app_demo/doctype/article/article.py
+++ my_app/app_demo/doctype/article/article.py
@@ -4,7 +4,24 @@
 # import frappe
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
-
+import requests
 class Article(WebsiteGenerator):
-    pass
+   def before_save(self):
+        """
+        Phương thức này sẽ được gọi trước khi một tài liệu (document) được lưu (create hoặc update).
+        """
+        # Thực hiện cuộc gọi API và xử lý dữ liệu nhận được
+        api_url = "https://jsonplaceholder.typicode.com/todos/1"
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            # Giả sử API trả về dữ liệu dưới dạng JSON và có trường 'desired_field'
+            api_data = response.json()
+            # desired_field_value = api_data.get('id')
+            # self.namesession = desired_field_value
+            # Gán giá trị vào trường cần thiết của doctype   
+        else:
+            frappe.throw(f"Failed to fetch data from API. Status code: {response.status_code}")
+            
+        
 	
